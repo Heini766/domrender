186e5df486bb1c3bc017655a3c765a9434a1f169
Heini766 <heinricha766@gmail.com>
2025-12-19 21:15:22 +0200

    Fix export syntax for utils in utilities.js

--- src/utils/utilities.js
+++ src/utils/utilities.js
@@ -1,4 +1,4 @@
-export default utils = {
+const utils = {
 
 getDistance: (pos1, pos2) => {
   const dx = pos2[0] - pos1[0]; // Difference in x-coordinates
@@ -163,3 +163,5 @@ getRelPos: (e) => {
 }
 
 }
+
+export default utils
